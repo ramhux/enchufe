6031287c58b95b928bca2ee85cd67b127c485863
Raul Morales <raul.morales-dev@bitbang.es>
2015-11-28 09:54:36 +0100

    Datagram class created

--- enchufe.py
+++ enchufe.py
@@ -1,4 +1,17 @@
 # enchufe.py
 # High-level socket API
 
-raise NotImplementedError
+class Datagram(object):
+    """UDP Datagram
+
+    A Datagram has:
+    * payload (data)
+    * Sourde address:port pair
+    * Destination address:port pair
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.src = kwargs.pop('src', None)
+        self.dst = kwargs.pop('dst', None)
+        self.payload = bytes(*args, **kwargs)
+        #FIXME check values
