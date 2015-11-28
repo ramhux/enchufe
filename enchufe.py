3182d65b94608167661f64bf588008d3ea9fcb62
Raul Morales <raul.morales-dev@bitbang.es>
2015-11-28 12:32:42 +0100

    Datagram docstring fix

--- enchufe.py
+++ enchufe.py
@@ -4,10 +4,10 @@
 class Datagram(object):
     """UDP Datagram
 
-    A Datagram has:
-    * payload (data)
-    * Sourde address:port pair
-    * Destination address:port pair
+    Datagram attributes
+    * payload: Binary data
+    * src: Source (address, port) pair
+    * dst: Destination (address, port) pair
     """
 
     def __init__(self, *args, **kwargs):
