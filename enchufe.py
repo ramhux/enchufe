4e0b0120abc0d5d15e6f9020cc457f5cd9ea5c01
Raul Morales <raul.morales-dev@bitbang.es>
2015-11-28 10:30:53 +0100

    Datagram object basic methods

--- enchufe.py
+++ enchufe.py
@@ -15,3 +15,31 @@ class Datagram(object):
         self.dst = kwargs.pop('dst', None)
         self.payload = bytes(*args, **kwargs)
         #FIXME check values
+
+    def __repr__(self):
+        r = 'Datagram({!r}'.format(self.payload)
+        if self.src is not None:
+            r += ', src={!r}'.format(self.src)
+        if self.dst is not None:
+            r += ', dst={!r}'.format(self.dst)
+        r += ')'
+        return r
+
+    def __bytes__(self):
+        return self.payload
+
+    def __eq__(self, other):
+        if isinstance(other, Datagram):
+            if self.payload != other.payload:
+                return False
+            if self.src != other.src:
+                return False
+            if self.dst != other.dst:
+                return False
+            return True
+        else:
+            return NotImplemented
+
+    # Mutable class, no hash
+    #def __hash__(self):
+    #    return hash((self.payload, self.src, self.dst))
