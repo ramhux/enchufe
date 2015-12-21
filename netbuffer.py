d8101f8142376b7252c27d08e17694d64068fa2e
Raul Morales <raul.morales-dev@bitbang.es>
2015-12-21 09:27:56 +0100

    NetBuffer static method -> module functions

--- netbuffer.py
+++ netbuffer.py
@@ -1,90 +1,83 @@
 # netbuffer.py
 # Helper for network data managing
 
+_encode = lambda s, codec: s.encode() if codec is None else s.encode(codec)
+_decode = lambda d, codec: d.decode() if codec is None else d.decode(codec)
+
+
+def block(data, size=512):
+    if size is None:
+        size = len(data)
+    return bytes(data[:size]), size
+
+def from_int(value, size=None, signed=None):
+    """Convert integer to bytes
+
+    size = 0 or None --> size is calculated automatically
+    size = X --> X bytes size"""
+    size = 0 if size is None else size
+    signed = value < 0 if signed is None else signed
+    if size < 0:
+        raise ValueError('Invalid size', size)
+    if size == 0:
+        size = value.bit_length()
+        size += 1 if signed else 0
+        size = ((size - 1) // 8) + 1
+    return value.to_bytes(size, 'big', signed=signed)
+
+def to_int(data, size=None, signed=None):
+    """Convert bytes to integer
+
+    Return the integer value and number of bytes used"""
+    size = 1 if size is None else size
+    signed = False if signed is None else signed
+    value = int.from_bytes(data[:size], 'big', signed=signed)
+    return value, size
+
+def from_str(value, size=None, encoding=None):
+    """Convert string to bytes
+
+    size = -X --> X bytes used as integer header for size
+    size = 0 --> encoded '\x00' added to the end of the bytes after encoding
+    size = X --> first X bytes only, filled with 0x00 if needed
+    """
+    bstr = _encode(value, encoding)
+    if size is None:
+        return bstr
+    length = len(bstr)
+    if size < 0:
+        size = -size
+        bstr = from_int(length, size) + bstr
+    elif size == 0:
+        bstr = bstr + _encode('\x00', encoding)
+    elif size > 0:
+        if size > length:
+            bsrt += b'\x00' * (size - length)
+        else:
+            bstr = bstr[:size]
+    return bstr
+
+def to_str(data, size=None, encoding=None):
+    if size is None:
+        value = _decode(data, encoding)
+        size = len(data)
+    if size < 0:
+        strsize, size = to_int(data, -size)
+        data = data[size:size+strsize]
+        value = _decode(data, encoding)
+        size += strsize
+    elif size == 0:
+        NUL = _encode('\x00', encoding)
+        head, sep, tail = data.partition(NUL)
+        value = _decode(head, encoding)
+        size = len(head) + len(sep)
+    elif size > 0:
+        data = data[:size]
+        value = _decode(data, encoding)
+    return value, size
 
 class NetBuffer(bytearray):
     """Parse and forge network data"""
-
-    ##### Static Methods #####
-    _encode = lambda s, codec: s.encode() if codec is None else s.encode(codec)
-    _decode = lambda d, codec: d.decode() if codec is None else d.decode(codec)
-
-    @staticmethod
-    def from_int(value, size=None, signed=None):
-        """Convert integer to bytes
-
-        size = 0 or None --> size is calculated automatically
-        size = X --> X bytes size"""
-        size = 0 if size is None else size
-        signed = value < 0 if signed is None else signed
-        if size < 0:
-            raise ValueError('Invalid size', size)
-        if size == 0:
-            size = value.bit_length()
-            size += 1 if signed else 0
-            size = ((size - 1) // 8) + 1
-        return value.to_bytes(size, 'big', signed=signed)
-
-    @staticmethod
-    def to_int(data, size=None, signed=None):
-        """Convert bytes to integer
-
-        Return the integer value and number of bytes used"""
-        size = 1 if size is None else size
-        signed = False if signed is None else signed
-        value = int.from_bytes(data[:size], 'big', signed=signed)
-        return value, size
-
-    @staticmethod
-    def from_str(value, size=None, encoding=None):
-        """Convert string to bytes
-
-        size = -X --> X bytes used as integer header for size
-        size = 0 --> encoded '\x00' added to the end of the bytes after encoding
-        size = X --> first X bytes only, filled with 0x00 if needed
-        """
-        bstr = NetBuffer._encode(value, encoding)
-        if size is None:
-            return bstr
-        length = len(bstr)
-        if size < 0:
-            size = -size
-            bstr = NetBuffer.from_int(length, size) + bstr
-        elif size == 0:
-            bstr = bstr + NetBuffer._encode('\x00', encoding)
-        elif size > 0:
-            if size > length:
-                bsrt += b'\x00' * (size - length)
-            else:
-                bstr = bstr[:size]
-        return bstr
-
-    @staticmethod
-    def to_str(data, size=None, encoding=None):
-        if size is None:
-            value = NetBuffer._decode(data, encoding)
-            size = len(data)
-        if size < 0:
-            strsize, size = NetBuffer.to_int(data, -size)
-            data = data[size:size+strsize]
-            value = NetBuffer._decode(data, encoding)
-            size += strsize
-        elif size == 0:
-            NUL = NetBuffer._encode('\x00', encoding)
-            head, sep, tail = data.partition(NUL)
-            value = NetBuffer._decode(head, encoding)
-            size = len(head) + len(sep)
-        elif size > 0:
-            data = data[:size]
-            value = NetBuffer._decode(data, encoding)
-        return value, size
-
-    @staticmethod
-    def from_bytes_to_bytes(data, size=None):
-        if size is None:
-            size = len(data)
-        return bytes(data[:size]), size
-    ##### End of Static Methods #####
 
     def __init__(self, *args, **kwargs):
         self.int_size = kwargs.pop('int_size', None)
@@ -123,22 +116,22 @@ class NetBuffer(bytearray):
             if isinstance(item['value'], int):
                 item.setdefault('size', self.int_size)
                 item.setdefault('signed', self.int_signed)
-                return self.from_int(**item)
+                return from_int(**item)
             if isinstance(item['value'], str):
                 item.setdefault('size', self.str_size)
                 item.setdefault('encoding', self.str_encoding)
-                return self.from_str(**item)
+                return from_str(**item)
 
         else: # a sequence (positional arguments)
             item = list(item)
             if isinstance(item[0], int):
                 if len(item) < 2: item.append(self.int_size)
                 if len(item) < 3: item.append(self.int_signed)
-                return self.from_int(*item)
+                return from_int(*item)
             if isinstance(item[0], str):
                 if len(item) < 2: item.append(self.str_size)
                 if len(item) < 3: item.append(self.str_encoding)
-                return self.from_str(*item)
+                return from_str(*item)
 
         # bad luck
         raise ValueError('Unexpected object', item)
@@ -153,11 +146,11 @@ class NetBuffer(bytearray):
             item['data'] = self
             if t is bytes:
                 item.setdefault('size', self.bytes_size)
-                return self.from_bytes_to_bytes(**item)
+                return block(**item)
             if t is int:
                 item.setdefault('size', self.int_size)
                 item.setdefault('signed', self.int_signed)
-                return self.to_int(**item)
+                return to_int(**item)
             if t is str:
                 item.setdefault('size', self.str_size)
                 item.setdefault('encoding', self.str_encoding)
@@ -172,7 +165,7 @@ class NetBuffer(bytearray):
             if t is int:
                 if len(item) < 2: item.append(self.int_size)
                 if len(item) < 3: item.append(self.int_signed)
-                return self.to_int(*item)
+                return to_int(*item)
             if t is str:
                 if len(item) < 2: item.append(self.str_size)
                 if len(item) < 3: item.append(self.str_encoding)
